1ed4f700d34385a04f537b75cedc053bf08a1beb
Carlos <carlos.izquierdo@iutcm.edu.ve>
2023-05-01 18:03:38 -0400

    agregando el ejercicio LeerTeclado

---
+++ src/ejercicios/LeerTeclado.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicios;
+import java.util.Scanner;
+/**
+ *
+ * @author Carlos
+ */
+public class LeerTeclado {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("ingrese su  nombre");
+        String nombre = teclado.nextLine();
+        System.out.println("Su nombre es "+nombre);
+    }
+    
+}
